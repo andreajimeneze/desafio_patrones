6960448ab73352f04f94ae0e96b5998047be7b94
Andrea Jiménez <andreacjimenez@gmail.com>
2024-04-28 12:03:03 -0400

    Agregar selección patrón

--- src/PatronesAnidados.java
+++ src/PatronesAnidados.java
@@ -3,34 +3,30 @@ import java.util.Scanner;
 public class PatronesAnidados {
     public static void main(String[] args) {
 
-        int num = ingresarNumero();
-//        mostrarPatron1(num);
-//        mostrarPatron2(num);
-//        mostrarPatron3(num);
-//        mostrarPatron4(num);
-          mostrarPatron5(num);
-    }
+        int numPatron;
 
-//    public static void elegirPatron() {
-//        Scanner sc = new Scanner(System.in);
-//
-//    }
-
-    public static void mostrarPatron5(int num) {
-        for(int i = 1; i <= num; i++) {
-            for(int j = 1; j <= num; j++) {
-                if((i == 1 && j < num) || (i == num && j > 1)) {
-                    System.out.printf("*");
-                } else if (i > 1 && i < num && j > 1 && j < num)  {
-                    System.out.printf("*");
-                } else {
-                    System.out.printf(" ");
-                }
-            }
-            System.out.println();
+        do {
+            System.out.println("Comience la selección de patrones. Seleccione un número entre 1 y 4:");
+            numPatron = ingresarNumero();
+        } while (numPatron >= 4);
+            elegirPatron(numPatron);
         }
-    }
-    public static void mostrarPatron1(int num) {
+
+   public static void elegirPatron(int numPatron) {
+   System.out.println("Ingrese un número");
+    int num = ingresarNumero();
+               if(numPatron == 1) {
+            mostrarPatron1(num);
+        } else if (numPatron == 2) {
+            mostrarPatron2(num);
+        } else if (numPatron == 3) {
+            mostrarPatron3(num);
+        } else if (numPatron == 4) {
+            mostrarPatron4(num);
+        }
+   }
+
+   public static void mostrarPatron1(int num) {
         for (int i = 1; i <= num; i++) {
             for (int j = 1; j <= num; j++) {
                 if (i == 1 || i == num || j == 1 || j == num) {
@@ -70,27 +66,23 @@ public class PatronesAnidados {
     }
 
     public static void mostrarPatron4(int num) {
-        for (int i = 1; i < num; i++) {
-            for (int j = 1; j <= num; j++) {
-                if (i == num || j == 1 || i > 1 && i < num || j >= num - 1) {
+        for(int i = 1; i <= num; i++) {
+            for(int j = 1; j <= num; j++) {
+                if((i == 1 && j < num) || (i == num && j > 1)) {
                     System.out.printf("*");
-                } else if (i == num && j == i) {
-                    System.out.printf("");
+                } else if (i > 1 && i < num && j > 1 && j < num)  {
+                    System.out.printf("*");
                 } else {
                     System.out.printf(" ");
                 }
             }
             System.out.println();
-            }
-
-                }
-
-
+        }
+    }
 
 
     public static int ingresarNumero() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresa un número");
         int num = sc.nextInt();
         return num;
     }
